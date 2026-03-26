026a5dd31ac0579c5692761b904abbd9f46e0b8e
Calebe Camilo <calebecamilo@hotmail.com>
2026-03-26 20:42:03 -0300

    Delete ex3.7.py

--- ex3.7.py
+++
@@ -1,5 +0,0 @@
-#Exercício 3.7
-n1 =  int(input("Digite o Primeiro número inteiro"))
-n2 =  int (input("Digite o segundo número inteiro"))
-soma = n1 + n2
-print("A soma dos dois números é igual a:", soma)
